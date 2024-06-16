c0aee635289c1dc5a985ca4cb3e8ba6b3d6840b9
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-16 14:34:08 +0600

    route for make any user admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -50,6 +50,19 @@ async function run() {
 
     app.get("/allUsers", async (req, res) => {
       const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/allUsers/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
