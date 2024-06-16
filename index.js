631bfecce3f700b7d2685d30d5bbd187d181cb2f
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-16 20:03:31 +0600

    make a route for user delete api

--- index.js
+++ index.js
@@ -94,6 +94,15 @@ async function run() {
       res.send(result);
     });
 
+    // handle user delete route
+    app.delete("/allUsers/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
