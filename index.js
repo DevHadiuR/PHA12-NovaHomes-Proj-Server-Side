1abeecd0498dfda8e2455a969fb31642b22e7047
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-16 11:01:41 +0600

    PH Assignment 12 - NovaHomes Project , server site basic initial setup done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleweare
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Nova Homes Server Site Activate!");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port :", port);
+});
