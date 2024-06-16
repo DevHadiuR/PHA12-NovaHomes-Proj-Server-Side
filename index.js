2ecd07df85235dd1e6dd3eebcdbfe596c04560c9
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-16 13:53:13 +0600

    make route for getting all the users info

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/allUsers", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
