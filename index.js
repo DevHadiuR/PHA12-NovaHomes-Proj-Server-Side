72e0dfa39e36d4eb3d6a105dd5ecc96e5fe4916b
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-18 20:10:29 +0600

    make a route for update any property data from database

--- index.js
+++ index.js
@@ -141,6 +141,24 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/allProperties/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProperty = {
+        $set: {
+          ...body,
+        },
+      };
+      const result = await propertyCollection.updateOne(
+        query,
+        updatedProperty,
+        options
+      );
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
