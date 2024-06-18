14fbf528f52bfd67ba5b6e68ea64f125f814ff94
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-18 20:33:13 +0600

    make a route for deleting property by agent from database

--- index.js
+++ index.js
@@ -159,6 +159,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/allProperties/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await propertyCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
