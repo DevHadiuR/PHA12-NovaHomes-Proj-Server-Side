fe933a7b9f3c3698109632da0ea8c5acad91b62d
Hadiur Rahman <hadiurahman139@gmail.com>
2024-06-19 18:08:18 +0600

    make a route for user review delete method

--- index.js
+++ index.js
@@ -242,6 +242,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/allReviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await reviewCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
